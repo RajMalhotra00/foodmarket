58d12f9026c4d475117c5a3632995aca2fcdaf57
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-01-18 15:29:30 +0100

    Initial Commit

---
+++ foodmarket/foodmarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodmarketApp.swift
+//  foodmarket
+//
+//  Created by Raj Malhotra on 18.01.21.
+//
+
+import SwiftUI
+
+@main
+struct foodmarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
